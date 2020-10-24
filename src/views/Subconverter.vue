dcd72bf75949f3067eb5b59dec4030afa69857d8
NZESupB <szboyxjw@gmail.com>
2020-10-24 17:14:02 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -301,7 +301,7 @@ export default {
         ]
       },
       form: {
-        sourceSubUrl: "",
+        sourceSubUrl: "https://raw.githubusercontent.com/NZESupB/SubConver-Clash-Profile/master/NetEaseSub",
         clientType: "",
         customBackend: "",
         remoteConfig: "",
